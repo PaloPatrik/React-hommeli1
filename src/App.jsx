3e22fc7327d9f7a77096f572a5b9a86969abc5f8
PaloPatrik <patrikpp06@gmail.com>
2023-08-24 09:57:38 +0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,83 +1,36 @@
 import { useState } from 'react'
 
-const StatisticLine = (props) => {
-  return(
-    <tr><td>{props.text + ' ' + props.value}</td></tr>
-  )
-}
-
-const Statistics = (props) => {
-  const {good, neutral, bad, all} = props;
-  const average = (good + (neutral * 0) + -bad) / all;
-  let positive = (props.good * 100) / props.all
-  return(
-    <tbody>
-      <StatisticLine text="good" value = {good} />
-      <StatisticLine text="neutral" value = {neutral} />
-      <StatisticLine text="bad" value = {bad} />
-      <StatisticLine text="all" value = {all} />
-      <StatisticLine text="average" value = {average} />
-      <StatisticLine text="positive" value = {positive} />
-    </tbody>
-  )
-}
-
 const Button = (props) => {
-  console.log(props)
-  return (
-    <button onClick={props.func}>{props.text}</button>
+  return(
+  <button onClick={props.seuraava}>Next</button>
   )
 }
-
 const App = () => {
-  const [good, setGood] = useState(0)
-  const [neutral, setNeutral] = useState(0)
-  const [bad, setBad] = useState(0)
-  const all = good + neutral + bad
+  const anecdotes = [
+    'If it hurts, do it more often.',
+    'Adding manpower to a late software project makes it later!',
+    'The first 90 percent of the code accounts for the first 90 percent of the development time...The remaining 10 percent of the code accounts for the other 90 percent of the development time.',
+    'Any fool can write code that a computer can understand. Good programmers write code that humans can understand.',
+    'Premature optimization is the root of all evil.',
+    'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.',
+    'Programming without an extremely heavy use of console.log is same as if a doctor would refuse to use x-rays or blood tests when dianosing patients.',
+    'The only way to go fast, is to go well.'
+  ]
 
-  const hyvaPalaute = () => {
-    setGood(good + 1)
-  }
-
-  const neutraaliPalaute = () => {
-    setNeutral(neutral + 1)
-  }
-
-  const huonoPalaute = () => {
-    setBad(bad + 1)
+  const [selected, setSelected] = useState(0)
+  let seuraava = () => {
+    let satunnainen = Math.floor((Math.random() * anecdotes.length) + 1)
+    setSelected(satunnainen)
   }
   
-  if (good === 0 && neutral === 0 && bad === 0) {
-    return(
-      <div>
-          <h2>Anna palautetta</h2>
-          <Button text='good' func={hyvaPalaute} />
-          <Button text='neutral' func={neutraaliPalaute} />
-          <Button text='bad' func={huonoPalaute} />
-        <h2>Tilastot</h2>
-        <p>
-          Ei palautetta annettu
-        </p>
-      </div>
-    )
-  } else {
-    return (
-      
-        <table>
-        <thead><tr><th>Anna palautetta</th></tr></thead>
-        <tbody>
-        <tr>
-          <td><Button text='good' func={hyvaPalaute} /></td>
-          <td><Button text='neutral' func={neutraaliPalaute} /></td>
-          <td><Button text='bad' func={huonoPalaute} /></td>
-        </tr>
-        </tbody>
-        <thead><tr><th>Tilastot</th></tr></thead>
-        <Statistics good={good} neutral={neutral} bad={bad} all={all} />
-        </table>
-      
-    )
-  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <br/>
+      <Button seuraava={seuraava} />
+    </div>
+  )
 }
 
 export default App
