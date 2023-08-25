f01d401466205cbba882e95ba21d85c28152e1b7
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2023-08-25 09:14:38 +0300

    1.14

--- src/App.jsx
+++ src/App.jsx
@@ -21,22 +21,51 @@ const App = () => {
   let seuraava = () => {
     let satunnainen = Math.floor((Math.random() * anecdotes.length) + 1)
     setSelected(satunnainen)
+    enitenAania();
   }
   const [points, setPoints] = useState(new Uint8Array(anecdotes.length));
+  const [aanet, setAanet] = useState(0)
+  const [position, setPosition] = useState(0);
 
   const handleVote = () => {
     let copy = [...points];
     copy[selected] += 1;
     setPoints(copy);
+    enitenAania();
+  }
+
+  const enitenAania = () => {
+    let eniten = points[0]
+    let position = 0;
+
+    for (let j = 0; j < points.length; j++) {
+      if (points[j] === 0){
+        if (points[j] > eniten){
+          eniten = points[j];
+          position = j;
+        }
+      }else{
+        if (points[j] > eniten -1){
+            eniten = points[j]
+            position = j;
+        }
+      } 
+    }
+    setAanet(eniten);
+    setPosition(position);
   }
 
   return (
     <div>
+      <h2>Anecdote of the day</h2>
       {anecdotes[selected]}
       <br/>
       has {points[selected]} votes!
       <button onClick={handleVote}>vote</button>
       <Button seuraava={seuraava} />
+      <h2>Anecdote with most votes</h2>
+      <i>{anecdotes[position]}</i>
+      <b>{points[selected] === aanet + 1? points[selected] : aanet}</b> <p>votes! </p>
     </div>
   )
 }
