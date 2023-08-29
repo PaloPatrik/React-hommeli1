abb8e07517168c40d9bb36274be3d27c52b9882f
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2023-08-29 11:52:58 +0300

    2.8

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,42 @@
 import { useState } from 'react'
 
+const checkValidity = (obj, arr) => {
+  for (let j = 0; j < arr.length; j++) {
+    if(arr[j].name === obj.name, arr[j].number === obj.number){
+      return true;
+    }
+  }
+  return false;
+}
+
 const App = () => {
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040 2867465'  }
   ]) 
   const [newName, setNewName] = useState('')
 
   const handleSubmit = (event) => {
     event.preventDefault()
-    if (newName.indexOf((newName) !== -1)) {
+
+    const obj = {name: newName, number: newNumber};
+
+    if (checkValidity(obj, persons)) {
       alert(`${newName} is already added to phonebook`)
+      alert(`${newNumber} is already added to phonebook`)
     }else{
-      setPersons([...persons, {name: newName}])
+      setPersons([...persons, obj])
     }
   }
+
+  const [newNumber, setNewNumber] = useState('')
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form onSubmit={handleSubmit}>
         <div>
-          name: <input onChange={(e) => setNewName(e.target.value)}/>
+          name: <input onChange={(e) => setNewName(e.target.value)}/><br/>
+          number: <input onChange={(e) => setNewNumber(e.target.value)}/>
         </div>
         <div>
           <button type="submit">add</button>
@@ -28,7 +44,8 @@ const App = () => {
       </form>
       <h2>Numbers</h2>
       {persons.map((persons) => 
-      <p>{persons.name}</p>)}
+      <p>{persons.name  + " "}
+      {persons.number}</p>)}
     </div>
   )
 }
