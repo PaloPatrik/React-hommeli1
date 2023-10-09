c19dff3e8bd035799b81a4dec8a0546cf3a36746
PaloPatrik <115207974+PaloPatrik@users.noreply.github.com>
2023-10-09 15:56:10 +0300

    Thtlista tehty

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
 const { useState } = require("react")
 
 const App = () => {
-  const [maara, setMaara] = useState(0);
-  
+ const [tehtava, setTehtava] = useState([]);
+ const [arvo, setArvo] = useState('');
+ const lisays = () => {
+  let copy = tehtava;
+copy.push(arvo)
+setTehtava(copy)
+setArvo('')
+ }
+ const poista = (testi) => {
+  for (let q = 0; q < tehtava.length; q++) {
+    const element = tehtava[q];
+    console.log(element,testi)
+    if (element === testi) {
+      let copy = [...tehtava];
+      copy.splice(q,1)
+      setTehtava(copy)
+      break
+    }
+  }
+ }
+ 
+
+ 
   return (
   <div>
-  <button onClick={() =>setMaara(maara -1)}>-</button>
-  <p>{maara}</p>
-  <button onClick={() =>setMaara(maara +1)}>+</button>
+    <input type="text" onChange={ (e) => setArvo(e.target.value)}/>
+    <button onClick={lisays}>Lisää</button>
+    <p>Tehtävät:</p>
+    {tehtava.map(testi => <p>{testi}<button onClick={() => poista(testi)}>Poista</button></p>)}
   </div>
 )
   }
